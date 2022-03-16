999c020c7e9a5f8cf0ba87296217b6da29462216
AmazoiEmre <amazoisftwr@gmail.com>
2022-03-16 11:24:55 +0300

    sf

--- public/client.js
+++ public/client.js
@@ -82,6 +82,16 @@ window.addEventListener('load', function() {
 		
 	});//END_SOCKET.ON
 	
+	 socket.on('ON_UPDATE_SOUND', function(id,name) {
+	     var currentUserAtr = id+':'+name;
+		 	
+		 if(window.unityInstance!=null)
+		{
+		   window.unityInstance.SendMessage ('NetworkManager', 'OnUpdateSound',currentUserAtr);
+		}
+		
+	});//END_SOCKET.ON
+	
 	
 		        
 	socket.on('USER_DISCONNECTED', function(id) {
